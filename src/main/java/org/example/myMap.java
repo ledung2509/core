47efe8a1c939e0354ad5177e55b8f012ee270439
ledungb12509@gmai.com <ledungb12509@gmail.com>
2025-06-11 11:00:17 +0700

    up_map

--- src/main/java/org/example/myMap.java
+++ src/main/java/org/example/myMap.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -42,6 +43,14 @@ public class myMap<K,V> implements Map<K,V> {
 
     @Override
     public V put(K key, V value) {
+        for (Entry<K,V> entry : list) {
+            if (entry.getKey().equals(key)) {
+                V oldValue = entry.getValue();
+                entry.setValue(value);
+                return oldValue;
+            }
+        }
+        list.add(new AbstractMap.SimpleEntry<>(key, value));
         return null;
     }
 
@@ -52,7 +61,9 @@ public class myMap<K,V> implements Map<K,V> {
 
     @Override
     public void putAll(Map<? extends K, ? extends V> m) {
-
+        for (Entry<? extends K, ? extends V> entry : m.entrySet()) {
+            put(entry.getKey(), entry.getValue());
+        }
     }
 
     @Override
